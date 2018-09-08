b07db7e917895d260d5c12ccca1c510e12c55664
Lemii <peter.spaargaren@gmail.com>
2018-09-08 11:29:03 +0200

    Add better comments

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
-# Node configuration
+# Node configuration, used for decode mode (retreiving data)
 # Tip: Use "https://testnet.lisk.io" (without ":7000") if you don't have your own node
 node = "http://127.0.0.1:7000"
 
-# Passphrase(s) of address to send TX from. Only applicable to "encode" mode.
+# Passphrase(s) of address to send TX from. Only applicable for "encode" mode.
 first_passphrase = "word word word word word word word word word word word word"
 second_passphrase = "false"
 
-# Default lisk-php folder
+# Default lisk-php folder. Required for encode mode.
 lisk_php_folder = "/home/lisk/lisk-php/"
 
-# Default values for 'recipient' and 'amount' if no arguments are given
+# Default values for 'recipient' and 'amount' if no arguments (-r, -a) are given
 recipient = "4560518704624832050L"
 amount = 0.00000001
